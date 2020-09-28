add229a74c49afd4a2aa167ea1a0238ba169f8c8
David Ng <david.ng@enett.com>
2020-09-28 18:08:37 +1000

    Modify Kestrel to listen on PORT env var

--- FitFinder.Grpc/Startup.cs
+++ FitFinder.Grpc/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,11 @@ namespace FitFinder.Grpc
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.Configure<KestrelServerOptions>(opt =>
+			{
+				opt.ListenAnyIP(Configuration.GetValue("PORT", 80));
+			});
+
 			services.AddGrpc();
 			services.RegisterGateway();
 			services.RegisterInfrastructure(Configuration);
